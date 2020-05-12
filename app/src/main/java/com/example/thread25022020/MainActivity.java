da526d7a7aa2ba704427d78a9d1621dd04742c87
phatdroid94 <phatdroid94@gmail.com>
2020-05-12 20:25:39 +0700

    init

--- app/src/main/java/com/example/thread25022020/MainActivity.java
+++ app/src/main/java/com/example/thread25022020/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.thread25022020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Khoi tao ra luong
+        final Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Log.d("BBB","Hello main");
+            }
+        });
+        thread.start();
+        CountDownTimer countDownTimer = new CountDownTimer(1000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Log.d("BBB",thread.getState().name());
+            }
+        };
+        countDownTimer.start();
+
     }
 }
